811de400bd8d8853d1b92552852b9aa8bc064211
LilianaPMPersonal <159660223+LilianaPMPersonal@users.noreply.github.com>
2024-02-10 18:30:06 -0500

    Update search.spec.ts

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,19 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('/');
+  await page.goto('');
 });
 
 test('Realizar una busqueda que no tenga resultados', async ({ page }) => {
-  await page.getByRole('button').click();
+  await page.getByRole('button',{name:'Search '}).click();
 
   await page.getByPlaceholder('Search docs').click();
 
   await page.getByPlaceholder('Search docs').fill('hascontent');
 
-  expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
+  expect(page.locator('.DocSearch-Help')).toBeVisible();
 
-  expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for hascontent');
+  expect(page.locator('.DocSearch-Help')).toHaveText('No recent searches');
+
 
 })
 
@@ -26,23 +27,25 @@ test('Limpiar el input de busqueda', async ({ page }) => {
 
   await searchBox.fill('somerandomtext');
 
-  await expect(searchBox).toHaveText('somerandomtext');
+  const inputText = await page.locator('Input.DocSearch-Input').inputValue();
+  await expect(inputText).toContain('somerandomtext');
 
   await page.getByRole('button', { name: 'Clear the query' }).click();
 
   await expect(searchBox).toHaveAttribute('value', '');
+  
 });
 
 test('Realizar una busqueda que genere al menos tenga un resultado', async ({ page }) => {
   await page.getByRole('button', { name: 'Search ' }).click();
-
+  await page.pause();
   const searchBox = page.getByPlaceholder('Search docs');
 
   await searchBox.click();
 
   await page.getByPlaceholder('Search docs').fill('havetext');
-
-  expect(searchBox).toHaveText('havetext');
+  const inputText = await page.locator('Input.DocSearch-Input').inputValue();
+  await expect(inputText).toContain('havetext');
 
   // Verity there are sections in the results
   await page.locator('.DocSearch-Dropdown-Container section').nth(1).waitFor();
